66a3e238f5ee61d07aca5f9ccfcc2a573bc29ef9
Adriano M4 <adriano@Adrianos-MacBook-Pro.local>
2025-08-07 11:19:58 +0200

    initial commit

---
+++ src/test/java/com/smoothstack/task_management_api/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.task_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
